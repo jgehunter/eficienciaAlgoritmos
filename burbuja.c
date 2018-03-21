510d00ab5f1b5361f6271f3c39d8708ed8380c99
Jorge James Hunter <Jorge James Hunter@JGEHUNTER>
2018-03-21 17:39:52 +0100

    Cambiado main para que se cree automaticamente una tabla de numeros aleatorios

--- burbuja.c
+++ burbuja.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include "burbuja.h"
+#include "algoritmos.h"
 
 void burbuja(struct lista tabla[], int elementos) {
 	int i, j;
 	struct lista aux;
 
-	for (i = 0; i < elementos - 1; i++) {
-		for (j = elementos - 1; j > i; j--) {
+	for (i = 0; i < elementos+1; i++) {
+		for (j = elementos; j > i; j--) {
 			if (tabla[j - 1].clave > tabla[j].clave) {
 				aux = tabla[j - 1];
 				tabla[j - 1] = tabla[j];
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
+#include "algoritmos.h"
 
 #define ELEM 20
 
@@ -9,15 +11,17 @@
 struct lista prueba[ELEM];
 
 void main() {
+	srand(time(NULL));
+
 	for (int i = 0; i <= ELEM; i++) {
-		prueba[i].clave = ELEM - i;
+		prueba[i].clave = rand();
 	}
 	
 	for (int i = 0; i <= ELEM; i++) {
 		printf("\nEl numero %d es: %d", i, prueba[i].clave);
 	}
 
-	burbuja(prueba, 5);
+	insercion(prueba, ELEM);
 
 	printf("\n");
 
