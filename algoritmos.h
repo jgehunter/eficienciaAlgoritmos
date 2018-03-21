de112fece5eb641c832437a0dc689ea48cd0f96a
inakietxeba <i.echevarriaherrera@gmail.com>
2018-03-21 22:00:22 +0100

    solo falla monticulo

--- algoritmos.h
+++ algoritmos.h
@@ -2,17 +2,17 @@
 #define ALGORITMOS_H
 #include "main.h"
 
-void burbuja(struct lista*, int elementos);
+void burbuja(struct lista*, int);
 
-void insercion(struct lista*, int elementos);
+void insercion(struct lista*, int);
 
-void seleccion(struct lista*, int elementos);
+void seleccion(struct lista*, int);
 
-void shell(struct lista*, int elementos);
+void shell(struct lista*, int);
 
 void criba( int, int);
 
-void monticulo(struct lista*, int elementos);
+void monticulo( struct lista*, int);
 
 void quicksort( int, int, struct lista*);
 
