b4995d245205f7702b2c1341c0f8092336d5c6c0
jgehunter <jgehunter@gmail.com>
2018-03-21 17:05:53 +0100

    Update algoritmos.h

--- algoritmos.h
+++ algoritmos.h
@@ -1,10 +1,13 @@
-#ifndef BURBUJA_H
-#define BURBUJA_H
+#ifndef ALGORITMOS_H
+#define ALGORITMOS_H
 #include "main.h"
 
 void burbuja(struct lista*, int elementos);
 
 void insercion(struct lista*, int elementos);
 
+void seleccion(struct lista*, int elementos);
+
 #endif
+
 
