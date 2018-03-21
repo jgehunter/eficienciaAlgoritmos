81f1dd1237434d5c9907837374fa9cc92975dbd8
jgehunter <jgehunter@gmail.com>
2018-03-21 17:07:17 +0100

    Add files via upload

---
+++ seleccion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "algoritmos.h"
+
+void seleccion(struct lista tabla[], int elementos) {
+	struct lista aux;
+	int i, j, menor;
+
+	for (i = 0; i < elementos; i++) {
+		menor = i;
+		for (j = i + 1; j <= elementos; j++) {
+			if (tabla[j].clave < tabla[menor].clave) {
+				menor = j;
+			}
+			if (menor != i) {
+				aux = tabla[i];
+				tabla[i] = tabla[menor];
+				tabla[menor] = aux;
+			}
+		}
+	}
+}
