268d80cbbb35aed2573e4c27fd9c1cb07bf4e16c
jgehunter <jgehunter@gmail.com>
2018-03-23 22:00:55 +0100

    Consolidado tiempo
    Consolidado las funciones de medida de tiempo en dos archivos

---
+++ measureTime.h
@@ -0,0 +1,25 @@
+#ifndef MEASURE_TIME_H
+
+#define MEASURE_TIME_H
+
+#ifdef _WIN32
+#include <Windows.h>
+extern LARGE_INTEGER tcounter, freq, tick_value, prev_tick_value;
+extern long long usecs;
+#elif defined(__APPLE__)
+#include "timeApple.h"
+#include <mach/mach_time.h>
+#include <stdint.h>
+extern uint64_t freq_num, freq_denom, tick_value, prev_tick_value;
+#elif defined(__linux__)
+#include <time.h>
+#include <sys/time.h>
+#include <stdint.h>
+extern long long prevTime, time;
+#endif
+
+void startTimer();
+
+long long getTime();
+
+#endif
