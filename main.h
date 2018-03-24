90056ff32aa8188acfa87d3722548fc00597cfb4
jgehunter <jgehunter@gmail.com>
2018-03-24 16:45:01 +0100

    Add files via upload

--- main.h
+++ main.h
@@ -9,16 +9,4 @@ struct lista {
     /*  char *last_name;*/
 };
 
-#ifdef _WIN32
-#include "timeWindows.h"
-#endif
-
-#ifdef __APPLE__
-#include "timeApple.h"
-#endif
-
-#ifdef _linux
-#include "timeLinux.h"
-#endif
-
 #endif
